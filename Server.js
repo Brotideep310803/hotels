6e3331cac4df1d6f24afcf0b9c09e200851f51d7
Brotideep <your-email@example.com>
2025-03-07 11:10:51 +0530

    shift mongodb to online server

--- Server.js
+++ Server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-
+const PORT = process.env.PORT || 3000;
 
 
 
@@ -17,6 +18,8 @@ app.use('/menu',menuRoutes);
 app.get('/',function(req,res){
     res.send('Welcome to my hotel...How i can help you ??,We have list of menus')
 })
+
+
 
 app.listen(3000, ()=>{
     console.log('listening on port 3000');
